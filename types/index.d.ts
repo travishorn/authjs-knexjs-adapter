921afc26c7653d7523c8e4cbcb3cb65350dc4e55
Travis Horn <travis@travishorn.com>
2023-05-18 10:41:23 -0500

    Add KnexAdapter type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,5 @@
 declare module 'authjs-knexjs-adapter' {
   // Import and re-export the types from the adapter file
-  import { Adapter, Knex } from "../index.ts";
-  export { Adapter, Knex };
+  import { Adapter, Knex, KnexAdapter } from "../index.ts";
+  export { Adapter, Knex, KnexAdapter };
 }
