ab568b3bcb13d467e5fc2f0c63eb3998333f898e
Travis Horn <travis@travishorn.com>
2023-06-14 11:35:06 -0500

    Remove migration from src comments

--- index.ts
+++ index.ts
@@ -3,75 +3,9 @@ import type { Knex } from "knex";
 
 export { Adapter, Knex };
 
-/*
-Auth.js requires a specific database schema. Use the following Knex.js
-migration to create it. Note: This migration is configured for MariaDB. It may
-work out-of-the-box with other database engines, or you may need to modify it
-according to your application.
-
-export const up = (knex) => {
-  return knex.schema
-    .createTable("User", (table) => {
-      table.uuid("id").defaultTo(knex.raw("(UUID())"));
-      table.text("name");
-      table.text("email").unique();
-      table.timestamp("emailVerified");
-      table.text("image");
-
-      table.primary("id");
-    })
-    .createTable("Session", (table) => {
-      table.uuid("id").defaultTo(knex.raw("(UUID())"));
-      table.timestamp("expires");
-      table.text("sessionToken").notNullable().unique();
-      table.uuid("userId");
-
-      table.primary("id");
-
-      table.foreign("userId").references("id").on("User");
-    })
-    .createTable("Account", (table) => {
-      table.uuid("id").defaultTo(knex.raw("(UUID())"));
-      table.uuid("userId");
-      table.text("type").notNullable();
-      table.text("provider").notNullable();
-      table.text("providerAccountId").notNullable();
-      table.text("refresh_token");
-      table.text("access_token");
-      table.bigInteger("expires_at");
-      table.text("token_type");
-      table.text("scope");
-      table.text("id_token");
-      table.text("session_state");
-
-      table.primary("id");
-
-      table.unique(["provider", "providerAccountId"]);
-
-      table.foreign("userId").references("id").on("User");
-    })
-    .createTable("VerificationToken", (table) => {
-      table.text("identifier");
-      table.string("token", 255);
-      table.timestamp("expires").notNullable();
-
-      table.primary("token");
-
-      table.unique(["token", "identifier"]);
-    });
-};
-
-export const down = (knex) => {
-  return knex.schema
-    .dropTable("VerificationToken")
-    .dropTable("Account")
-    .dropTable("Session")
-    .dropTable("User");
-};
-*/
-
 /**
- * A Next-Auth adapter for Knex.js
+ * An adapter for Auth.js/NextAuth.js to allow you to connect to any database
+ * service via Knex.js.
  *
  * @param {Knex} knex - The Knex.js connection
  * @param options - Auth.js options
